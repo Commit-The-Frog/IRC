cf5282ffabab2e8f1fce4a5a41f31edf8e5604dd
Sol Jeong <soljeong@c2r4s6.42seoul.kr>
2024-06-28 16:29:15 +0900

    feat: server test 완

---
+++ Channel.hpp
@@ -0,0 +1,15 @@
+class Channel
+{
+	private:
+		operator_list;
+		user_list;
+		invite_list;
+		option_map; // bool값으로 옵션 적용되어있는지 확인
+		key;
+		topic;
+		
+		// 처음 조인한 애는 무조건 오퍼레이터임!
+		// 
+
+
+}
--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include "command/Command.hpp"
 #include "command/Pass.hpp"
 #include "parser/Parser.hpp"
+#include "Server.hpp"
 #include <vector>
 #include <sstream>
 
-enum cmd_type {
-	PASS, NICK, USER
-};
+// enum cmd_type {
+// 	PASS, NICK, USER
+// };
 
 void init(map<int, Command*>& cmd_map, map<int, Client>& client_map, string& server_pwd) {
 	cmd_map[PASS] = new Pass(client_map, server_pwd);
@@ -20,27 +21,32 @@ void init(map<int, Command*>& cmd_map, map<int, Client>& client_map, string& ser
 	- run command
 */
 int main(int argc, char *argv[]) {
-	string server_pwd = "1234";
-	map<int, Client> client_map;
-	map<int, Command*> cmd_map;
-	map<int, Client>::iterator it;
+	if (argc != 3)
+		exit(1);
+	Server server = Server(atoi(argv[1]), argv[2]);
+	server.run();
 
-	// make mock client list
-	Client test1(3);
-	test1.setNickname("junkim2");
-	test1.setUsername("kim junho");
-	Client test2(4);
-	test2.setNickname("minjacho");
-	test2.setUsername("choi minjae");
-	client_map.insert(make_pair(3, test1));
-	client_map.insert(make_pair(4, test2));
+	// string server_pwd = "1234";
+	// map<int, Client> client_map;
+	// map<int, Command*> cmd_map;
+	// map<int, Client>::iterator it;
 
-	// set every elems in cmd_map
-	init(cmd_map, client_map, server_pwd);
-	// parse input
-	string tmp(argv[1]);
-	cmd_map[PASS]->execute(Parser(tmp), 3);
-	cout << client_map[3].getNickname() << " : " << client_map[3].getSendBuff() << endl;
+	// // make mock client list
+	// Client test1(3);
+	// test1.setNickname("junkim2");
+	// test1.setUsername("kim junho");
+	// Client test2(4);
+	// test2.setNickname("minjacho");
+	// test2.setUsername("choi minjae");
+	// client_map.insert(make_pair(3, test1));
+	// client_map.insert(make_pair(4, test2));
+
+	// // set every elems in cmd_map
+	// init(cmd_map, client_map, server_pwd);
+	// // parse input
+	// string tmp(argv[1]);
+	// cmd_map[PASS]->execute(Parser(tmp), 3);
+	// cout << client_map[3].getNickname() << " : " << client_map[3].getSendBuff() << endl;
 
 	// free
 	// delete cmd_map[PASS];
