00fd7feb4a9bbe195756111c800cf12a07cf38a1
minjacho42 <minjacho@student.42seoul.kr>
2024-07-02 14:56:05 +0900

    feat: unknown command handled

---
+++ command/Unknown.hpp
@@ -0,0 +1,21 @@
+#ifndef UNKNOWN_HPP
+# define UNKNOWN_HPP
+
+# include "Command.hpp"
+
+class Unknown: public Command {
+	public:
+		Unknown(map<int, Client>& client_map, map<string, Channel>& channel_map)
+		:Command(client_map, channel_map) {};
+		virtual void execute(const Parser& parser, int client_fd) {
+			Client&	client = this->client_map[client_fd];
+
+			if (!client.getIsRegistered()) {
+				return ;
+			} else {
+				client.setSendBuff(Reply::getCodeMsg("421", client.getNickname(), parser.getCmd() + " :Unknown command"));
+			}
+		}
+};
+
+#endif
