8c2b89022c85caaa9e2de1138d35ea371589a62b
Sol Jeong <soljeong@c2r4s6.42seoul.kr>
2024-07-02 20:05:26 +0900

    feat: change map value to pointer

--- channel/Channel.cpp
+++ channel/Channel.cpp
@@ -16,8 +16,9 @@ Channel &Channel::operator=(const Channel &channel) {
 }
 
 Channel::~Channel() {
-	for (std::map<string, Client>::iterator it = member_map.begin(); it != member_map.end(); ++it) {
-		it->second.deleteChannel(this->channel_name);
+	for (std::map<string, Client *>::iterator it = member_map.begin(); it != member_map.end(); it++) {
+		Client *client = it->second;
+		client->deleteChannel(this->channel_name);
 	}
 }
 
@@ -29,7 +30,7 @@ const string &Channel::getChannelName() const {
 	return this->channel_name;
 }
 
-void Channel::addOperator(const string &nick, const Client &client) {
+void Channel::addOperator(const string &nick, Client *client) {
 	operator_map[nick] = client;
 }
 
@@ -37,17 +38,17 @@ void Channel::deleteOperator(const string &nick) {
 	operator_map.erase(nick);
 }
 
-void Channel::addMember(const string &nick, Client &client) {
+void Channel::addMember(const string &nick, Client *client) {
 	member_map[nick] = client;
-	client.addChannel(this->channel_name, *this);
+	client->addChannel(this->channel_name, this);
 }
 
-void Channel::deleteMember(const string &nick, Client &client) {
+void Channel::deleteMember(const string &nick, Client *client) {
 	member_map.erase(nick);
-	client.deleteChannel(channel_name);
+	client->deleteChannel(channel_name);
 }
 
-void Channel::addInvite(const string &nick, const Client &client) {
+void Channel::addInvite(const string &nick, Client *client) {
 	inivite_map[nick] = client;
 }
 
@@ -55,15 +56,15 @@ void Channel::deleteInvite(const string &nick) {
 	inivite_map.erase(nick);
 }
 
-const std::map<string, Client> &Channel::getOperatorMap() const {
+const std::map<string, Client *> &Channel::getOperatorMap() const {
 	return operator_map;
 }
 
-const std::map<string, Client> &Channel::getMemberMap() const {
+const std::map<string, Client *> &Channel::getMemberMap() const {
 	return member_map;
 }
 
-const std::map<string, Client> &Channel::getInviteMap() const {
+const std::map<string, Client *> &Channel::getInviteMap() const {
 	return inivite_map;
 }
 
@@ -137,7 +138,7 @@ void Channel::setModeOptionL(bool flag) {
 }
 
 void Channel::sendAllClients(const string &msg) {
-	for (std::map<string, Client>::iterator it = member_map.begin(); it != member_map.end(); ++it) {
-		it->second.setSendBuff(msg);
+	for (std::map<string, Client *>::iterator it = member_map.begin(); it != member_map.end(); ++it) {
+		it->second->setSendBuff(msg);
 	}
 }
