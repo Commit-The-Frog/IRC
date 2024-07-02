29059f3225558bea2571a6c04d238ebce23f173a
minjacho42 <minjacho@student.42seoul.kr>
2024-07-02 14:55:31 +0900

    feat: Nick modified to check is passed

--- command/Nick.hpp
+++ command/Nick.hpp
@@ -20,6 +20,8 @@ class Nick: public Command
 			string			nick;
 			Client&			client = this->client_map[client_fd];
 
+			if (!client.getIsPassed())
+				return ;
 			if (params.size() == 0) {
 				client.setSendBuff(Reply::getCodeMsg("431", client.getNickname(), ":No nickname given"));
 			} else {
@@ -29,7 +31,19 @@ class Nick: public Command
 						client.setSendBuff(Reply::getCodeMsg("432", client.getNickname(), nick + " :Erroneus nickname"));
 						return ;
 					}
-					client.setNickname(nick);
+					if (!client.getIsRegistered()) {
+						// 초기 설정이 완료된 경우
+						client.setNickname(nick);
+						if (client.getIsRegistered()) {
+							client.setSendBuff(Reply::getCodeMsg("001", client.getNickname(), \
+								":Welcome to the FT_IRC Network, " + client.getNickname()));
+							cout << "connection established" << endl;
+						}
+					} else {
+						string reply = Reply::getCommonMsg(client, "NICK", ":"+nick);
+						client.setNickname(nick);
+						client.setSendBuff(reply);
+					}
 				} catch (Client::AlreadyInUseNickException& e) {
 					client.setSendBuff(Reply::getCodeMsg("433", client.getNickname(), nick + " :Nickname is already in use"));
 				} catch (Client::SameNickException& e) {}
